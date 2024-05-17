7299f2d16f68da48822dc991748d62cdf5ad1756
dev-nirob1 <dev-nirob1@gmail.com>
2024-05-17 08:07:49 +0600

    banner section complete

---
+++ src/pages/Home/Banner.jsx
@@ -0,0 +1,22 @@
+
+const Banner = () => {
+    return (
+        <section className="h-[70vh] w-full relative">
+            <div className="relative h-[70vh] bg-blue-500 text-[rgba(0, 0, 55, 0.5)]">
+                <div className="absolute inset-0 bg-cover bg-center" style={{
+                    backgroundImage: "linear-gradient(to bottom, rgba(0, 0, 105, 0.5), rgba(0, 0, 105, 0.5))," +
+                        "url('/banner.jpg')"
+                }}>
+                    <div className="flex items-center justify-center h-full">
+                        <div className="w-[480px] flex items-center">
+                            <input type="text" className="px-5 py-3 rounded-l-full outline-none w-full" placeholder="Enter the keyword..." />
+                            <button className="px-7 py-3 rounded-r-full bg-[#040404] font-medium text-gray-200 hover:bg-neutral-900">Search</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Banner;
